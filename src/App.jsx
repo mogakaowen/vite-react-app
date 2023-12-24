6699f030d9d99fdf4c7437faf48e4d893d727ea1
Owen Mogaka <owenmogaka@gmail.com>
2023-12-24 17:22:21 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import ErrorExample from "./tutorial/01-useState/errorExample";
+import UseRef from "./tutorial/07-useRef/useRef-basics";
+
+function App() {
+  return (
+    <div className="container">
+      <ErrorExample />
+    </div>
+  );
+}
+
+export default App;
