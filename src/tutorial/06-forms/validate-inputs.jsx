8fc16b3783b6582e93064cc70751621c8bcaf7ef
Owen Mogaka <owenmogaka@gmail.com>
2024-03-18 12:48:04 +0300

    validate

--- src/tutorial/06-forms/validate-inputs.jsx
+++ src/tutorial/06-forms/validate-inputs.jsx
@@ -25,39 +25,43 @@ const ValidateInputs = () => {
     setAge(e.target.value);
   };
 
+  const handleBlur = (field) => () => {
+    switch (field) {
+      case "name":
+        if (!name.trim()) {
+          setNameError("Name is required");
+        } else {
+          setNameError("");
+        }
+        break;
+      case "email":
+        if (!email.trim()) {
+          setEmailError("Email is required");
+        } else if (!/\S+@\S+\.\S+/.test(email)) {
+          setEmailError("Invalid email format");
+        } else {
+          setEmailError("");
+        }
+        break;
+      case "age":
+        if (!age.trim()) {
+          setAgeError("Age is required");
+        } else if (isNaN(age) || parseInt(age) <= 0) {
+          setAgeError("Invalid age");
+        } else {
+          setAgeError("");
+        }
+        break;
+      default:
+        break;
+    }
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Reset error messages
-    setNameError("");
-    setEmailError("");
-    setAgeError("");
-
-    // Validation logic
-    let isValid = true;
-
-    if (!name.trim()) {
-      setNameError("Name is required");
-      isValid = false;
-    }
-
-    if (!email.trim()) {
-      setEmailError("Email is required");
-      isValid = false;
-    } else if (!/\S+@\S+\.\S+/.test(email)) {
-      setEmailError("Invalid email format");
-      isValid = false;
-    }
-
-    if (!age.trim()) {
-      setAgeError("Age is required");
-      isValid = false;
-    } else if (isNaN(age) || parseInt(age) <= 0) {
-      setAgeError("Invalid age");
-      isValid = false;
-    }
 
     // If all validations pass, proceed with form submission
-    if (isValid) {
+    if (!nameError && !emailError && !ageError) {
       setIsFormValid(true);
 
       // You can add your logic here to handle the form data
@@ -87,6 +91,7 @@ const ValidateInputs = () => {
           className="form-input"
           value={name}
           onChange={handleNameChange}
+          onBlur={handleBlur("name")}
         />
         <div
           className="error-message"
@@ -106,6 +111,7 @@ const ValidateInputs = () => {
           className="form-input"
           value={email}
           onChange={handleEmailChange}
+          onBlur={handleBlur("email")}
         />
         <div
           className="error-message"
@@ -125,6 +131,7 @@ const ValidateInputs = () => {
           className="form-input"
           value={age}
           onChange={handleAgeChange}
+          onBlur={handleBlur("age")}
         />
         <div
           className="error-message"
