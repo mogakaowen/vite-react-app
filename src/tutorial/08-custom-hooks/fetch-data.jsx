6e059c0e20ae18bfe03eb93062f7f931b7dd1eac
mogakaowen <owenmogaka@gmail.com>
2024-06-05 10:14:54 +0300

    company

--- src/tutorial/08-custom-hooks/fetch-data.jsx
+++ src/tutorial/08-custom-hooks/fetch-data.jsx
@@ -20,7 +20,7 @@ const FetchData = () => {
         alt={name}
       />
       <h2>{name}</h2>
-      <h4>works at {company || "Equity Bank Limited"}</h4>
+      <h4>works at {company}</h4>
       <p>{bio}</p>
     </div>
   );
